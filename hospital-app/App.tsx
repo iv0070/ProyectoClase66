8b550327d72b83fb5156b76e1fdbc99b8ceecc7c
AshlyCruuz <anahlycruz23@gmail.com>
2026-08-31 18:41:33 -0600

    Crear PatientStackNavigator para dar acceso a AgendarCita desde las pestanas del paciente

--- hospital-app/App.tsx
+++ hospital-app/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import PatientTabs from './src/navigation/PatientTabs';
+import PatientStackNavigator from './src/navigation/PatientStackNavigator';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <PatientTabs />
+      <PatientStackNavigator />
     </NavigationContainer>
   );
 }
